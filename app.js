19c2e608d9b0b2ba71161385c7bbcda761419024
unknown <jpendy@gmail.com>
2021-01-18 22:09:41 -0600

    exercises done

---
+++ app.js
@@ -0,0 +1,71 @@
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+
+/* Write an ES2015 Version */
+
+function createInstructor(firstName, lastName) {
+	return {
+		firstName,
+		lastName
+	};
+}
+
+// var favoriteNumber = 42;
+
+// var instructor = {
+// 	firstName: 'Colt'
+// };
+
+// instructor[favoriteNumber] = 'That is my favorite!';
+
+/* Write an ES2015 Version */
+
+const favoriteNumber = 42;
+
+let instructor = {
+	firstname: 'Colt',
+	[favoriteNumber]: 'That is my favorite!'
+};
+
+// var instructor = {
+//     firstName: "Colt",
+//     sayHi: function(){
+//       return "Hi!";
+//     },
+//     sayBye: function(){
+//       return this.firstName + " says bye!";
+//     }
+//   }
+
+/* Write an ES2015 Version */
+
+var instructor = {
+	firstName: 'Colt',
+	sayHi() {
+		return 'Hi!';
+	},
+	sayBye() {
+		return this.firstName + ' says bye!';
+	}
+};
+
+const createAnimal = (species, verb, noise) => {
+	return {
+		species,
+		[verb]() {
+			return noise;
+		}
+	};
+};
+
+const d = createAnimal('dog', 'bark', 'Woooof!');
+// {species: "dog", bark: ƒ}
+d.bark(); //"Woooof!"
+
+const s = createAnimal('sheep', 'bleet', 'BAAAAaaaa');
+// {species: "sheep", bleet: ƒ}
+s.bleet(); //"BAAAAaaaa"
